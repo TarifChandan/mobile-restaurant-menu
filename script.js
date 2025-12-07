9bdcc974fccca8f238d4f0828949802bad05b9d3
Tarif Chandan <20030520+TarifChandan@users.noreply.github.com>
2025-12-07 19:03:33 +0600

    Implement add-to-cart Functionality

--- script.js
+++ script.js
@@ -1,4 +1,54 @@
-import menuArray from "/data.js";
+import { menuArray, savedOrders } from "/data.js";
+
+document.addEventListener("click", function (e) {
+  if (e.target.classList.contains("menu-card-add-btn")) {
+    saveOrders(e.target.dataset.menuId);
+    renderOrderSummary();
+  }
+});
+
+function saveOrders(selectedItemId) {
+  const selectedMenuItemObj = menuArray.find(
+    (menuItem) => menuItem.id === +selectedItemId
+  );
+
+  savedOrders.push(selectedMenuItemObj);
+}
+
+function getOrderSummaryHtml() {
+  let totalPrice = 0;
+
+  const orderSummaryHtml = savedOrders.map(function (menuItem) {
+    const { name, id, price } = menuItem;
+
+    totalPrice += price;
+    return `
+      <div class="order-item">
+          <span class="order-item-name">${name}</span>
+          <button class="order-item-remove">remove</button>
+          <span class="order-item-price">$${price}</span>
+      </div>`;
+  });
+
+  orderSummaryHtml.push(`
+    <div class="order-total">
+        <span class="order-total-title">Total price:</span>
+        <span class="order-total-price">$${totalPrice}</span>
+    </div>
+
+    <button class="place-order-btn">Complete order</button>
+  `);
+
+  orderSummaryHtml.unshift(
+    `<span class="order-summary-title">Your order</span>`
+  );
+
+  return orderSummaryHtml.join("");
+}
+
+function renderOrderSummary() {
+  document.querySelector(".order-summary").innerHTML = getOrderSummaryHtml();
+}
 
 function getMenuHtml() {
   return menuArray
@@ -9,10 +59,10 @@ function getMenuHtml() {
           <div class="menu-card-details">
             <span class="menu-card-title">${name}</span>
             <p class="menu-card-description">${ingredients.join(", ")}</p>
-            <span class="menu-card-price">${price}</span>
+            <span class="menu-card-price">$${price}</span>
           </div>
           <div class="menu-card-actions">
-            <button class="menu-card-add-btn">+</button>
+            <button class="menu-card-add-btn" data-menu-id="${id}">+</button>
           </div>
         </div>`;
     })
